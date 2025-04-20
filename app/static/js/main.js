fd93fd65777b85dc144afa14f5f848b3d6d4f172
T-Arancibia <135309866+T-Arancibia@users.noreply.github.com>
2025-04-20 03:48:03 -0400

    Create main.js
    Se comienza con la creación del JavaScript que enlaza el HTML con la app. En primera instancia, se extraen los elementos identificados previamente en index.html para posteriormente ser utilizados más adelante.

---
+++ app/static/js/main.js
@@ -0,0 +1,13 @@
+//Aqui se establecen los fetch hacia los endpoints de la api (routes.py)
+
+//Se rescatan los id de las secciones de index.html que registran informacion importante para realizar los fetch al back-end
+
+const loginSection = document.getElementById("pseudo-login");
+const chatSection = document.getElementById("seccion-chat");
+const usernameInput = document.getElementById("username");
+const loginBtn = document.getElementById("login-button");
+const historyList = document.getElementById("historial-chats");
+const chatbox = document.getElementById("chat-box");
+const form = document.getElementById("add-message");
+const messageInput = document.getElementById("message");
+const newConversationBtn = document.getElementById("nueva-conversacion");
